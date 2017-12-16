ec9bdb502a98e69b18388d0ece8524152acb416c
Vitalii Dmitriev <dmit.vitalii@gmail.com>
2017-12-16 03:29:15 +0300

    [android] move all android files to a new project.
    * Create an android project, which can be built separately and
      which can contain several inner modules;
    
    * Move all android modules to the one android project;
    
    * Add some timely useless kotlin files, related to tests;

---
+++ android/application/src/test/java/herac/org/tuxguitar/ExampleUnitTest.kt
@@ -0,0 +1,15 @@
+package herac.org.tuxguitar
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * This example test file is needed to commit the test dir structure.
+ */
+class ExampleUnitTest {
+    @Test
+    fun `addition is correct`() {
+        assertEquals(4, 2 + 2)
+    }
+}
