830b84bf4bfc124e971eb584c49945607f0a41e9
Vitalii Dmitriev <dmit.vitalii@gmail.com>
2018-03-18 20:16:54 +0300

    Remove everything! It's not working.

--- TuxGuitar-ui-toolkit/src/org/herac/tuxguitar/ui/widget/UIWrapLabel.java
+++
@@ -1,8 +0,0 @@
-package org.herac.tuxguitar.ui.widget;
-
-public interface UIWrapLabel extends UILabel {
-	
-	Float getWrapWidth();
-	
-	void setWrapWidth(Float wrapWidth);
-}
--- android/application/src/main/java/org/herac/tuxguitar/android/view/dialog/TGDialogController.java
+++
@@ -1,8 +0,0 @@
-package org.herac.tuxguitar.android.view.dialog;
-
-import android.app.Activity;
-
-public interface TGDialogController {
-	
-	void showDialog(Activity activity, TGDialogContext dialogContext);
-}
